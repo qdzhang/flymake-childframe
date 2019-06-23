f528f7cf96279b64082a26dc3bc4abfbce34f1b2
Junyi <junyi.hou@berkeley.edu>
2019-06-22 21:51:10 -0700

    bug fix

--- flymake-posframe.el
+++ flymake-posframe.el
@@ -160,6 +160,10 @@ Only need to run once.  Once run, remove itself from the hooks"
   (unless (eq (point) flymake-posframe--cursor-pos)
     (posframe-hide flymake-posframe-buffer)
 
+    ;; reset flymake-posframe--line
+    (setq-local flymake-posframe--line
+                (cons 0 0))
+
     (dolist (hook flymake-posframe-hide-posframe-hooks)
       (remove-hook hook #'flymake-posframe-hide))))
 
